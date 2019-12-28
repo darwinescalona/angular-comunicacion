aaa432208a4a3f1ea497ad6be3819a09ff7273f9
darwinescalona <darwin.escalona.trabajo@gmail.com>
2019-12-28 20:41:56 -0300

    add ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Store, Action } from '@ngrx/store';
+
+interface AppStore{
+  contador: number
+}
 
 @Component({
   selector: 'my-app',
@@ -9,15 +14,24 @@ export class AppComponent  {
   name = 'Angularer';
   contador: number;
 
-  constructor(){
-    this.contador = 10;
+  constructor(
+    //private store: Store<AppStore>
+    ){
+    //this.contador = 10;
+    /*this.store.subscribe(state =>{
+      console.log('state', state);
+    });*/
   }
 
   incrementar(){
-    this.contador++;
+    const action: Action = {
+      type: 'INCREMENTAR'
+    }
+    //this.store.dispatch(action);
+    //this.contador++;
   }
 
   decrementar(){
-    this.contador--;
+    //this.contador--;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+// NGRX
+import { StoreModule } from '@ngrx/store';
+
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { contadorReducer } from './contador/contador.reducer';
 import { NietoComponent } from './contador/nieto/nieto.component';
 import { PadreComponent } from './contador/padre/padre.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    StoreModule.forRoot({cont: contadorReducer})
+    ],
   declarations: [ AppComponent, HelloComponent, HijoComponent, PadreComponent, NietoComponent ],
   bootstrap:    [ AppComponent ]
 })
